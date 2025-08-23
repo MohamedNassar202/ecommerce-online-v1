9531b80cc3da2db85339d05b0afa7039d786f65d
MohamedNassar202 <nassarmohamed506@gmail.com>
2025-08-23 16:09:31 +0300

    Update user.routes.js

--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import * as user from "./controller/user.controller.js";
+import { allowedTo, protectedRoutes } from "../auth/controller/auth.controller.js";
 const userRouter = Router()
 
 userRouter.route('/')
     .post(user.addUser)
-    .get(user.getAllUsers)
+    .get(protectedRoutes, allowedTo('admin'), user.getAllUsers)
 userRouter.route('/:id')
     .put(user.updateUser)
     .patch(user.changeUserPassword)
