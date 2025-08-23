2a1c7cbe4ca895b1dcb9013017a8aca784d85cc9
MohamedNassar202 <nassarmohamed506@gmail.com>
2025-08-23 16:33:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors())
 app.use(express.json())
-app.use(express.static('/uploads')) 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 bootstrap(app)
 connection()
 
