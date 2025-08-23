fa79a370040b07c08682eecbcdce7dac918e31f9
MohamedNassar202 <nassarmohamed506@gmail.com>
2025-08-23 16:28:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors())
 app.use(express.json())
-app.use(express.static('uploads')) 
+app.use(express.static('/uploads')) 
 bootstrap(app)
 connection()
 
